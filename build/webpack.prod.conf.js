a4a1ba4a67f0454967897ed572aff9c576002715
abc1763613206 <abc1763613206@163.com>
2025-03-02 18:24:54 +0000

    feat: bump webpack to v4, replace several plugins

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -4,13 +4,14 @@ const path = require('path')
 const utils = require('./utils')
 const webpack = require('webpack')
 const config = require('../config')
-const merge = require('webpack-merge')
+const merge = require('webpack-merge').merge
 const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin")
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -25,56 +26,55 @@ const webpackConfig = merge(baseWebpackConfig, {
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          chunks: 'initial',
+          test: module => /node_modules/.test(module.resource),
+          minChunks: 1,
+          priority: 10
+        },
+        common: {
+          name: 'common',
+          chunks: chunk => ['oj', 'admin'].includes(chunk.name),
+          minChunks: 2
+        }
+      }
+    },
+    runtimeChunk: {
+      name: 'manifest'
+    },
+    concatenateModules: true
+  },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': config.build.env
     }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
 
     // extract css into its own file
-    new ExtractTextPlugin({
+    new MiniCssExtractPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
-      allChunks: true
+      chunkFilename: utils.assetsPath('css/[id].[contenthash].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
       cssProcessorOptions: {
-        safe: true
+      safe: true
       }
     }),
-    new UglifyJSPlugin({
+    new TerserPlugin({
       exclude: /\.min\.js$/,
       cache: true,
       parallel: true,
-      sourceMap: true
+      sourceMap: config.build.productionSourceMap
     }),
 
     // keep module.id stable when vender modules does not change
     new webpack.HashedModuleIdsPlugin(),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      chunks: ['oj', 'admin'],
-      minChunks: 2
-      // minChunks: function (module) {
-      // any required modules inside node_modules are extracted to vendor
-      // return (
-      //   module.resource &&
-      //   /\.js$/.test(module.resource) &&
-      //   module.resource.indexOf(
-      //     path.join(__dirname, '../node_modules')
-      //   ) === 0
-      // )
-      // }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
-    }),
     // copy custom static assets
     new CopyWebpackPlugin([
       {
@@ -113,8 +113,10 @@ const webpackConfig = merge(baseWebpackConfig, {
         // more options:
         // https://github.com/kangax/html-minifier#options-quick-reference
       }
-    })
-  ]
+    }),
+    new VueLoaderPlugin()
+  ],
+  mode: process.env.NODE_ENV !== 'production' ? 'development' : 'production',
 })
 
 if (config.build.productionGzip) {
