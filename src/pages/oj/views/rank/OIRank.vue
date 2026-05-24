67087ed0dacac07b1490aab83d386ba60cac39d5
turbileg <turbilegbatbileg@gmail.com>
2026-05-24 11:49:41 -0400

    fixedRank

--- src/pages/oj/views/rank/OIRank.vue
+++ src/pages/oj/views/rank/OIRank.vue
@@ -3,14 +3,11 @@
     <Col :span="22">
     <Panel :padding="10">
       <div slot="title">{{$t('m.OI_Ranklist')}}</div>
-      <div class="echarts">
-        <ECharts :options="options" ref="chart" auto-resize></ECharts>
-      </div>
+      <Table :data="dataRank" :columns="columns" size="large"></Table>
+      <Pagination :total="total" :page-size.sync="limit" :current.sync="page"
+                  @on-change="getRankData"
+                  show-sizer @on-page-size-change="getRankData(1)"></Pagination>
     </Panel>
-    <Table :data="dataRank" :columns="columns" size="large"></Table>
-    <Pagination :total="total" :page-size.sync="limit" :current.sync="page"
-                @on-change="getRankData"
-                show-sizer @on-page-size-change="getRankData(1)"></Pagination>
     </Col>
   </Row>
 </template>
@@ -88,66 +85,7 @@
               return h('span', utils.getACRate(params.row.accepted_number, params.row.submission_number))
             }
           }
-        ],
-        options: {
-          tooltip: {
-            trigger: 'axis'
-          },
-          legend: {
-            data: [this.$i18n.t('m.Score')]
-          },
-          grid: {
-            x: '3%',
-            x2: '3%'
-          },
-          toolbox: {
-            show: true,
-            feature: {
-              dataView: {show: true, readOnly: true},
-              magicType: {show: true, type: ['line', 'bar']},
-              saveAsImage: {show: true}
-            },
-            right: '10%'
-          },
-          calculable: true,
-          xAxis: [
-            {
-              type: 'category',
-              data: ['root'],
-              boundaryGap: true,
-              axisLabel: {
-                interval: 0,
-                showMinLabel: true,
-                showMaxLabel: true,
-                align: 'center',
-                formatter: (value, index) => {
-                  return utils.breakLongWords(value, 14)
-                }
-              },
-              axisTick: {
-                alignWithLabel: true
-              }
-            }
-          ],
-          yAxis: [
-            {
-              type: 'value'
-            }
-          ],
-          series: [
-            {
-              name: this.$i18n.t('m.Score'),
-              type: 'bar',
-              data: [0],
-              barMaxWidth: '80',
-              markPoint: {
-                data: [
-                  {type: 'max', name: 'max'}
-                ]
-              }
-            }
-          ]
-        }
+        ]
       }
     },
     mounted () {
@@ -156,34 +94,11 @@
     methods: {
       getRankData (page) {
         let offset = (page - 1) * this.limit
-        let bar = this.$refs.chart
-        bar.showLoading({maskColor: 'rgba(250, 250, 250, 0.8)'})
         api.getUserRank(offset, this.limit, RULE_TYPE.OI).then(res => {
-          if (page === 1) {
-            this.changeCharts(res.data.data.results.slice(0, 10))
-          }
           this.total = res.data.data.total
           this.dataRank = res.data.data.results
-          bar.hideLoading()
         })
-      },
-      changeCharts (rankData) {
-        let [usernames, scores] = [[], []]
-        rankData.forEach(ele => {
-          usernames.push(ele.user.username)
-          scores.push(ele.total_score)
-        })
-        this.options.xAxis[0].data = usernames
-        this.options.series[0].data = scores
       }
     }
   }
 </script>
-
-<style scoped lang="less">
-  .echarts {
-    margin: 0 auto;
-    width: 95%;
-    height: 400px;
-  }
-</style>
